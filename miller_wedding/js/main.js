614ece7b79b8b2605ee678d14fe5b26ebc2801a6
pennmeg <penn.meg@gmail.com>
2017-05-11 19:39:59 -0400

    Initial upload

---
+++ miller_wedding/js/main.js
@@ -0,0 +1,10 @@
+// Make it rain!
+
+$(window).on('scroll', function () {
+	var distanceScrolled = $(window).scrollTop();
+	console.log('The distance scrolled is: ' + distanceScrolled);
+// 	if ($(this).scrollTop() >= 541) {
+// 	   $('nav').addClass('scrolled');
+// } else {
+// 	   $('nav').removeClass('scrolled');
+// }
