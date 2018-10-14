dc69232949adce90a747e1687e504066ccf3db6d
Megan Penn <penn.meg@gmail.com>
2018-10-14 18:00:02 -0400

    Add files via upload

--- portfolio/script/scripts.js
+++ portfolio/script/scripts.js
@@ -1,21 +1,16 @@
 $(document).ready(function() {
-  console.log("== hello world ==");
-  var secondNav = $('#secondNav')
+  // $(function(){
+  //   var currentPageName = getPageName(window.location.pathname);
+  //   $('#' + currentPageName).addClass('active');
+  // });
   var topButton = $('#topButton')
   $(window).scroll(function() {
-    if($(window).scrollTop() > 56) {
-      // secondNav.css('visibility', 'visible');
-      secondNav.fadeIn(500);
-    } else {
-      // secondNav.css('visibility', 'hidden');
-      secondNav.fadeOut(500);
-    }
     if($(window).scrollTop() > 100) {
       topButton.css('visibility', 'visible');
     } else {
       topButton.css('visibility', 'hidden');
     }
-});
+  });
   $('#topButton').on('click', function(){
     console.log("-- click topButton --");
     window.scrollTo(0, 0);
